6e33aed3eb217ce1fdc5b33b9e0672343ca6df1e
ByTricuxs <150404506+ByTricuxs@users.noreply.github.com>
2024-04-30 21:26:10 -0600

    Arreglo a FoodWithImage, subi la clase vacia, jaja

--- Project_Restaurant/src/main/java/model/FoodWithImage.java
+++ Project_Restaurant/src/main/java/model/FoodWithImage.java
@@ -1,5 +1,36 @@
 package model;
 
-public class FoodWithImage {
+import org.json.simple.JSONObject;
 
+public class FoodWithImage {
+private Food food;
+    private String imagePath;
+
+    public FoodWithImage(Food food, String imagePath) {
+        this.food = food;
+        this.imagePath = imagePath;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    @SuppressWarnings("unchecked")
+    public JSONObject toJson() {
+        JSONObject jsonFoodWithImage = new JSONObject();
+        jsonFoodWithImage.put("food", food.toJson());
+        jsonFoodWithImage.put("imagePath", imagePath);
+        return jsonFoodWithImage;
+    }
+
+    public static FoodWithImage fromJson(JSONObject jsonFoodWithImage) {
+        JSONObject jsonFood = (JSONObject) jsonFoodWithImage.get("food");
+        Food food = Food.fromJson(jsonFood);
+        String imagePath = (String) jsonFoodWithImage.get("imagePath");
+        return new FoodWithImage(food, imagePath);
+    }
 }
