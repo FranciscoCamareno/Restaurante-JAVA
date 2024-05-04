98d135a7225699799385503fd2bc71aef2970654
ByTricuxs <150404506+ByTricuxs@users.noreply.github.com>
2024-05-04 13:46:01 -0600

    arreglo

--- Project_Restaurant/src/main/java/model/Food.java
+++ Project_Restaurant/src/main/java/model/Food.java
@@ -10,11 +10,16 @@ import org.json.simple.JSONObject;
  * @author valer
  */
 public class Food {
+
     private String idNumber;
     private String name;
     private String description;
     private double price;
-    private String category; // Nueva propiedad para la categoría
+    private String category;
+    private String imagePath;
+
+    public Food() {
+    }
 
     public Food(String idNumber, String name, String description, double price, String category) {
         this.idNumber = idNumber;
@@ -24,36 +29,63 @@ public class Food {
         this.category = category;
     }
 
-    // Getters y setters
-
-    @SuppressWarnings("unchecked")
-    public JSONObject toJson() {
-        JSONObject jsonFood = new JSONObject();
-        jsonFood.put("idNumber", idNumber);
-        jsonFood.put("name", name);
-        jsonFood.put("description", description);
-        jsonFood.put("price", price);
-        jsonFood.put("category", category);
-        return jsonFood;
+    public String getIdNumber() {
+        return idNumber;
     }
 
-    public static Food fromJson(JSONObject jsonFood) {
-        String idNumber = (String) jsonFood.get("idNumber");
-        String name = (String) jsonFood.get("name");
-        String description = (String) jsonFood.get("description");
-        double price = (Double) jsonFood.get("price");
-        String category = (String) jsonFood.get("category"); 
-        return new Food(idNumber, name, description, price, category);
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 
     @Override
     public String toString() {
-        return "Food{" +
-                "idNumber='" + idNumber + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", category='" + category + '\'' + 
-                '}';
+        return "Food{"
+                + "idNumber='" + idNumber + '\''
+                + ", name='" + name + '\''
+                + ", description='" + description + '\''
+                + ", price=" + price
+                + ", category='" + category + '\''
+                + ", imagePath='" + imagePath + '\''
+                + '}';
     }
 }
